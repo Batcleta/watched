feebdfda70b799a9a42a944468ee838e9c538738
Washington Ferreira <35137108+Batcleta@users.noreply.github.com>
2023-07-26 09:53:21 -0300

    styling login page

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -6,23 +6,22 @@ import { Observable, of, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class UserService {
   private readonly dbUrl = 'http://localhost:3004/users';
 
-  constructor(private httpClient: HttpClient,
-    private router: Router) { }
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
   login(user: userInfo) {
-
-    return this.userAuthentication(user).pipe(tap((response) => {
-      if (!response.success) return;
-      localStorage.setItem('token', btoa(JSON.stringify(response.token)));
-      localStorage.setItem('user', btoa(JSON.stringify(response.user)));
-      this.router.navigate(['']);
-    }))
+    return this.userAuthentication(user).pipe(
+      tap((response) => {
+        if (!response.success) return;
+        localStorage.setItem('token', btoa(JSON.stringify(response.token)));
+        localStorage.setItem('user', btoa(JSON.stringify(response.user)));
+        this.router.navigate(['']);
+      })
+    );
   }
 
   private userAuthentication(user: userInfo): Observable<any> {
@@ -40,8 +39,8 @@ export class UserService {
 
         if (authenticatedUser) {
           response.success = true;
-          response.user = authenticatedUser,
-            response.token = "mylittletoken";
+          (response.user = authenticatedUser),
+            (response.token = 'mylittletoken');
         }
 
         return response;
@@ -60,8 +59,6 @@ export class UserService {
 
   get loggeduser(): userInfo {
     let retrievedObject = localStorage.getItem('user');
-    return retrievedObject
-      ? JSON.parse(atob(retrievedObject))
-      : null;
+    return retrievedObject ? JSON.parse(atob(retrievedObject)) : null;
   }
 }
